f2e2beb0193d74aafa14a8e3691f9ef4d2e1e2b8
h4ever <yangyinhui163@163.com>
2020-07-13 22:43:12 +0800

    maven project build

---
+++ src/test/java/solutions/array/PascalsTriangleII119Test.java
@@ -0,0 +1,15 @@
+package test.java.solutions.array;
+
+import org.junit.Test;
+import main.java.solutions.array.PascalsTriangleII119;
+
+import static org.junit.Assert.*;
+
+public class PascalsTriangleII119Test {
+
+    @Test
+    public void getRow() {
+        PascalsTriangleII119 testInstance = new PascalsTriangleII119();
+        System.out.println(testInstance.getRow2(6));
+    }
+}
